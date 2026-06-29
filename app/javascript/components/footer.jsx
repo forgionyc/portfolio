a9a557e31c68aa5af32dd189a7c5db411507f974
Forgiony <carlos.forgiony@gmail.com>
2026-06-29 18:03:44 -0500

    working footer

--- app/javascript/components/footer.jsx
+++ app/javascript/components/footer.jsx
@@ -4,8 +4,8 @@ import reactSvg from '/assets/react.svg'
 
 export default function Footer({ first_name, last_name }) {
   return (
-    <footer className="flex flex-col px-20 py-22">
-      <div className="flex justify-between items-center">
+    <footer className="flex flex-col mt-auto border-t border-t-[rgb(30,30,33)]">
+      <div className="flex justify-between items-center px-10 py-12">
         <h1 className="font-space-grotesk font-medium text-[52px] tracking-[-1.04px] leading-[54.6px]">Let's build something <br></br> durable. <a href={`mailto:${first_name.toLowerCase()}.${last_name.toLowerCase()}@gmail.com`} className="text-[oklch(0.8_0.15_155)]">
   {first_name.toLowerCase()}.{last_name.toLowerCase()}@gmail.com →</a>
         </h1>
@@ -14,26 +14,26 @@ export default function Footer({ first_name, last_name }) {
           <li className="hover:text-[rgb(237,237,238)] transition-colors duration-300"><a href="/linkedin">LINKEDIN</a></li>
         </ul>
       </div>
-      {/* <div className="mt-auto">
-        <p>Made with ❤️ with RoR <a href="https://rubyonrails.org" target="_blank">
+      <div className="flex justify-center font-jetbrains-mono text-[12px] mb-4">
+        <p>Made with ❤️ using RoR <a href="https://rubyonrails.org" target="_blank">
           <img
-            className="w-5 h-5 inline-block h-[9.8vw] min-h-[130px] [will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_rgb(211_0_1/0.6))] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))]"
+            className="w-5 h-5 inline-block [will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_rgb(211_0_1/0.6))] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))]"
             alt="Ruby on Rails Logo"
             src={railsSvg} />
           </a> + Inertia.js <a href="https://inertia-rails.dev" target="_blank">
             <img
-              className="w-5 h-5 inline-block h-[9.8vw] min-h-[130px] will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_#646cffaa)] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))]"
+              className="w-5 h-5 inline-block will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_#646cffaa)] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))]"
               src={inertiaSvg}
               alt="Inertia logo" />
           </a> + React.js <a href="https://react.dev" target="_blank">
             <img
-              className="w-5 h-5 inline-block h-[9.8vw] min-h-[130px] [will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_#61dafbaa)] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))] motion-safe:animate-[logo-spin_20s_linear_infinite]"
+              className="w-5 h-5 inline-block [will-change:filter] transition-[filter] duration-300 [filter:drop-shadow(0_20px_13px_rgb(0_0_0/0.03))_drop-shadow(0_8px_5px_rgb(0_0_0/0.08))] hover:[filter:drop-shadow(0_0_2em_#61dafbaa)] dark:[filter:drop-shadow(0_20px_13px_rgb(255_255_255/0.03))_drop-shadow(0_8px_5px_rgb(255_255_255/0.08))] motion-safe:animate-[logo-spin_20s_linear_infinite]"
               src={reactSvg}
               alt="React logo"
             />
           </a>
         </p>
-      </div> */}
+      </div>
     </footer>
   )
 }
--- app/javascript/pages/portfolio/index.jsx
+++ app/javascript/pages/portfolio/index.jsx
@@ -2,9 +2,9 @@ import Navbar from '../../components/navbar'
 import Footer from '../../components/footer'
 export default function Index({ first_name, last_name, message }) {
   return (
-    <div className="bg-[rgb(10,10,11)] text-[rgb(237,237,238)] min-h-screen antialiased">
+    <div className="flex flex-col bg-[rgb(10,10,11)] text-[rgb(237,237,238)] min-h-screen antialiased">
       <Navbar first_name={first_name} last_name={last_name}/>
-      <h1 className="flex justify-center border-b border-b-[rgb(30,30,33)] p-20">{message}</h1>
+      <p className="flex justify-center p-20">{message}</p>
       <Footer first_name={first_name} last_name={last_name}/>
     </div>
   )
